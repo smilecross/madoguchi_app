93b58d70ea196f18facae912951126faa4d6fd9d
smilecross <crossheart44@gmail.com>
2023-09-10 09:48:04 +0900

    update md file

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\Chat;
+use Auth;
+use App\Models\User;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chats = Chat::getAllOrderByUpdated_at();
+        return response()->view('chat.index',compact('chats'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return response()->view('chat.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         // バリデーション
+        $validator = Validator::make($request->all(), [
+            'chat' => 'required | max:191',
+            'description' => 'required',
+        ]);
+        // バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect()
+            ->route('chat.create')
+            ->withInput()
+            ->withErrors($validator);
+        }
+        // create()は最初から用意されている関数
+        // 戻り値は挿入されたレコードの情報
+        $data = $request->merge(['user_id' => Auth::user()->id])->all();
+        // $result = Chat::create($date);
+          $result = Chat::create($request->all());
+        // ルーティング「chat.index」にリクエスト送信（一覧ページに移動）
+        return redirect()->route('chat.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $chat = Chat::find($id);
+        return response()->view('chat.show', compact('chat'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $chat = Chat::find($id);
+        return response()->view('chat.edit', compact('chat'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'chat' => 'required | max:191',
+            'description' => 'required',
+        ]);
+        //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect()
+            ->route('chat.edit', $id)
+            ->withInput()
+            ->withErrors($validator);
+        }
+        //データ更新処理
+        $result = Chat::find($id)->update($request->all());
+        return redirect()->route('chat.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $result = Chat::find($id)->delete();
+        return redirect()->route('chat.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
+// use App\Http\Controllers\FavoriteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::middleware(['web'])->resource('chat', ChatController::class);
+
+Route::middleware('auth')->group(function () {
+  Route::post('chat/{chat}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+  Route::post('chat/{chat}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+
+//   Route::get('/chat/mypage', [ChatController::class, 'mydata'])->name('chat.mypage');
+
+  Route::resource('chat', ChatController::class);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
